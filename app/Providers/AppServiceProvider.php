926295ec6461bb84a679de6137bd23bc172e1e2a
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2022-06-02 13:48:16 +0000

    fix: Ao rodar migrate no servidor de producao aconteceu isso https://github.com/laravel/framework/issues/17508

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+	Schema::defaultStringLength(191);
+	
         if ($this->app->environment('local')) {
             Mail::alwaysTo(env('MAIL_DEV_TEST'));
         }
