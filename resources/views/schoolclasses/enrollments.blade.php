5a40010e677808475120dd6533e4f41bae5ebed9
Ayrton Douglas Freitas Guimarães <ayrtondouglas@usp.br>
2023-03-14 09:37:47 -0300

    fix: foi corrigido o download dos historicos dos semestres anteriores

--- resources/views/schoolclasses/enrollments.blade.php
+++ resources/views/schoolclasses/enrollments.blade.php
@@ -47,7 +47,7 @@
                             <td class="text-center">
                                 <form method="POST" action="{{ route('schoolrecords.download') }}" target="_blank">
                                     @csrf
-                                    <input type='hidden' name='path' value="{{ $inscricao->student->getSchoolRecordFromOpenSchoolTerm()->file_path }}">
+                                    <input type='hidden' name='path' value="{{ $inscricao->student->schoolrecords()->whereBelongsTo($inscricao->schoolclass->schoolterm)->first()->file_path }}">
                                     <button class="btn btn-link"
                                         data-toggle="tooltip" data-placement="top"
                                         title="Download"
